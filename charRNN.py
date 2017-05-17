6e7d5b55be11fa2adaeb17fe569bad8d4b7e1408
Frank Schneider <frank.stefan.schneider@gmail.com>
2017-05-17 16:58:09 +0200

    read in data and create batches

--- charRNN.py
+++ charRNN.py
@@ -6,8 +6,12 @@ import tensorflow as tf
 
 ### Global Variables ###
 
-DATA_PATH = 'Data/Shakespeare.txt'
-READ_VOCAB = True # If True, the vocabulary of characters will be read from the data
+DATA_PATH = 'Data/Shakespeare_edit.txt'
+READ_VOCAB = False # If True, the vocabulary of characters will be read from the data
+SEQ_LENGTH = 10 # Length of Sequence, Input to the RNN, (= size of X)
+OVERLAP = SEQ_LENGTH/2 # Size of the overlap between the different Sequences
+BATCH_SIZE = 4 # Batch size (=number of Sequences per "batch")
+
 
 
 
@@ -22,19 +26,34 @@ def define_vocab():
 		vocab = list(sorted(set(data)))
 	else:
 		vocab = list(sorted(set("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            "abcdefghijklmnopqrstuvwxyz"
-            " .,!?"
-            "0123456789")))
+			"abcdefghijklmnopqrstuvwxyz"
+			" .,!?"
+			"0123456789")))
 	vocab_size = len(vocab)
 	print 'Vocabulary :', vocab,'\n'
 	print 'Size of the Vocabulary:', vocab_size, 'characters','\n'
 	return vocab, vocab_size
 
 
+def read_data(char2idx):
+	seq = []
+	for text in open(DATA_PATH, 'r').read():
+		idx = char2idx[text]
+		seq.append(idx)
+		if len(seq) == SEQ_LENGTH:
+			yield seq
+			seq = seq[SEQ_LENGTH-OVERLAP:SEQ_LENGTH]
 
+def read_batch(data_stream):
+	batch = []
+	for seq in data_stream:
+		batch.append(seq)
+		if len(batch) == BATCH_SIZE:
+			yield batch
+			batch = []
+	yield batch
 
-
-
+# [map(idx2char.get, s) for s in batch] "retranslate a whole batch"
 
 
 ### Main ###
@@ -43,8 +62,7 @@ def main():
 	vocab, vocab_size = define_vocab()
 	char2idx = { ch:i for i,ch in enumerate(vocab) }
 	idx2char = { i:ch for i,ch in enumerate(vocab) }
-	print char2idx['A']
-	print idx2char[5]
+
 
 if __name__ == '__main__':
 	main()
